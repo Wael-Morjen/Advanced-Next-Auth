59982e833380f38db135a1e39a2080de98418893
Wael-Morjen <waelmorjen@gmail.com>
2024-01-14 19:27:17 +0100

    RegisterSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
         message: "Password is required"
     }),
 })
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(6, {
+        message: "Password requires 6 characters"
+    }),
+    name: z.string().min(1, {
+        message: "Name is required"
+    })
+})
