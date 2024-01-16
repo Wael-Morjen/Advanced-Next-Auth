e3719a5193807fb36080abadd1403d3c7e9517a0
Wael-Morjen <waelmorjen@gmail.com>
2024-01-16 16:48:19 +0100

    link accounts

--- auth.ts
+++ auth.ts
@@ -6,12 +6,30 @@ import { getUserById } from "./data/user"
 import { db } from "./lib/db"
 import authConfig from "./auth.config"
 
+declare module 'next-auth' {
+    interface User {
+        role?: UserRole;
+    }
+}
+
 export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
 } = NextAuth({
+    pages: {
+        signIn: "/auth/login",
+        error: "/auth/error"
+    },
+    events: {
+        async linkAccount({ user }) {
+            await db.user.update({
+                where: { id: user.id },
+                data: { emailVerified: new Date() },
+            })
+        }
+    },
     callbacks: {
         async session({ token, session }) {
             if (token.sub && session.user) {
