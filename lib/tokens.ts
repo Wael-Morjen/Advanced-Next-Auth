269bbd9e45674f5661ea9a203c480ea024cc6a97
Wael-Morjen <waelmorjen@gmail.com>
2024-01-17 17:22:43 +0100

    generatePasswordResetToken

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,31 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token"
 import { v4 as uuidv4 } from "uuid"
 
 import { db } from "./db";
+
+export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getPasswordResetTokenByEmail(email);
+
+    if (existingToken) {
+        await db.passwordResetToken.delete({
+            where: { id: existingToken.id}
+        });
+    }
+
+    const passwordResetToken = await db.passwordResetToken.create({
+        data: {
+            email,
+            token,
+            expires
+        },
+    });
+
+    return passwordResetToken;
+}
 
 export const generateVerificationToken = async (email: string) => {
     const token = uuidv4();
