93e5427662a92c8ca61f1d1c248f70f52977dea6
Wael-Morjen <waelmorjen@gmail.com>
2024-01-17 15:26:08 +0100

    send confirmation email

--- actions/login.ts
+++ actions/login.ts
@@ -1,12 +1,15 @@
 "use server";
 
-import * as z from "zod"
+import * as z from "zod";
 
 import { LoginSchema } from "@/schemas";
 
-import { signIn } from "@/auth"
+import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
+import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -16,6 +19,20 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     }
 
     const { email, password } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+
+    if (!existingUser || !existingUser.email || !existingUser.password) {
+        return { error: "Email does not exist" }
+    }
+
+    if (!existingUser.emailVerified) {
+        const verificationToken = await generateVerificationToken(existingUser.email);
+
+        await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+        return { success: "Confirmation email sent!" };
+    }
 
     try {
        await signIn("credentials", {
--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,8 @@ import * as z from "zod"
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values);
@@ -31,7 +33,9 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         },
     });
 
-    // TODO: Send verification token email 
+    const verificationToken = await generateVerificationToken(email);
 
-    return { success: "User created"};
+    await sendVerificationEmail(verificationToken.email, verificationToken.token)
+
+    return { success: "Confirmation email has been sent!"};
 };
