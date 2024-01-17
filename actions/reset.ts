8e7fa6efc9882619c9e9637932bf7a5df00127d8
Wael-Morjen <waelmorjen@gmail.com>
2024-01-17 17:21:18 +0100

    reset password action

---
+++ actions/reset.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import * as z from "zod";
+
+import { ResetSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+import { generatePasswordResetToken } from "@/lib/tokens";
+import { sendPasswordResetEmail } from "@/lib/mail";
+
+export const reset = async (values: z.infer<typeof ResetSchema>) => {
+    const validatedFields = ResetSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+        return { error: "Invalid email" };
+    }
+
+    const { email } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+
+    if (!existingUser) {
+        return { error: "Email not found" };
+    }
+
+    const passwordResetToken = await generatePasswordResetToken(email);
+    await sendPasswordResetEmail(passwordResetToken.email, passwordResetToken.token);
+
+    return { success: "Reset email has been sent !"};
+}
