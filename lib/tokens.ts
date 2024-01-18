61324ab6a76894bf5b01656b15f1b327bd974bce
Wael-Morjen <waelmorjen@gmail.com>
2024-01-18 14:33:06 +0100

    generateTwoFActorToken

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,8 +1,34 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token"
-import { v4 as uuidv4 } from "uuid"
-
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { db } from "./db";
+
+import { v4 as uuidv4 } from "uuid"
+import crypto from "crypto";
+
+export const generateTwoFActorToken = async (email: string) => {
+    const token = crypto.randomInt(100_000, 1_000_000).toString();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+    const existingToken = await getTwoFactorTokenByEmail(email);
+
+    if (existingToken) {
+        await db.twoFactorToken.delete({
+            where: { id: existingToken.id },
+        });
+    }
+
+    const twoFActorToken = await db.twoFactorToken.create({
+        data: {
+            email,
+            token,
+            expires,
+        }
+    });
+
+    return twoFActorToken;
+}
+
 
 export const generatePasswordResetToken = async (email: string) => {
     const token = uuidv4();
