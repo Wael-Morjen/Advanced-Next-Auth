6d0e3e2d1c35acd017361c319b61f5c004635e3a
Wael-Morjen <waelmorjen@gmail.com>
2024-01-20 17:11:50 +0100

    callback url for social buttons

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -3,14 +3,18 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 import { Button } from "../ui/button";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("callbackUrl")
+
     const onClick = (provider: "google" | "github") => {
         signIn(provider, {
-            callbackUrl: DEFAULT_LOGIN_REDIRECT
+            callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT
         })
     }
 
