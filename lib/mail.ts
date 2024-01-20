a30ef80b5ecae11ab14bec54ef865daa5b626dc6
Wael-Morjen <waelmorjen@gmail.com>
2024-01-20 17:13:32 +0100

    minor changes

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,11 +2,13 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const domain = process.env.NEXT_PUBLIC_APP_URL;
+
 export const sendVerificationEmail = async (
     email: string,
     token: string,
 ) => {
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
     const htmlContent = `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
@@ -39,7 +41,7 @@ export const sendPasswordResetEmail = async (
     email: string,
     token: string
 ) => {
-    const resetLink = `http://localhost:3000/auth/new-password?${token}`;
+    const resetLink = `${domain}/auth/new-password?token=${token}`;
 
     const htmlContent = `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
