c07accdbe354643074c65ad32581152c3b4742e7
Wael-Morjen <waelmorjen@gmail.com>
2024-01-20 17:11:03 +0100

    implement the modal

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,6 +1,12 @@
 'use client';
 
 import { useRouter } from "next/navigation";
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger
+} from "@/components/ui/dialog"
+import { LoginForm } from "./LoginForm";
 
 interface LoginButtonProps {
     children: React.ReactNode;
@@ -22,9 +28,14 @@ export const LoginButton = ({
 
     if (mode === "modal") {
         return (
-            <span>
-                {/*TODO: Implement modal*/}
-            </span>
+            <Dialog>
+                <DialogTrigger asChild={asChild}>
+                    {children}
+                </DialogTrigger>
+                <DialogContent className="p-0 w-auto bg-transparent border-none">
+                    <LoginForm />
+                </DialogContent>
+            </Dialog>
         )
     }
 
