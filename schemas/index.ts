a1a40c1b31f232a977a64119c93dde3e681d1f0d
Wael-Morjen <waelmorjen@gmail.com>
2024-01-20 17:14:08 +0100

    SettingsSchema & NewPasswordSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,32 @@
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
+
+export const SettingsSchema = z.object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+}).refine((data) => {
+    if (data.password && !data.newPassword) {
+        return false
+    }
+
+    return true
+}, {
+    message: "New password is required",
+    path: ["newPassword"]
+}).refine((data) => {
+    if (data.newPassword && !data.password) {
+        return false
+    }
+
+    return true
+}, {
+    message: "password is required",
+    path: ["password"]
+})
 
 export const LoginSchema = z.object({
     email: z.string().email({
@@ -27,3 +55,9 @@ export const ResetSchema = z.object({
         message: "Email is required"
     }),
 })
+
+export const NewPasswordSchema = z.object({
+    password: z.string().min(6, {
+        message: "Minimum of 6 characters required",
+    }),
+});
