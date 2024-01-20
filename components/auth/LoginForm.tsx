9d7cb75ac72802dae3c193a64d77c7349a506ee3
Wael-Morjen <waelmorjen@gmail.com>
2024-01-20 17:11:23 +0100

    callback url history

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -26,6 +26,8 @@ import Link from "next/link";
 
 export const LoginForm = () => {
     const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("callbackUrl")
+
     const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
         ? "Email already in use with different provider"
         : "";
@@ -48,7 +50,7 @@ export const LoginForm = () => {
         setSuccess("");
 
         startTransition(() => {
-            login(values)
+            login(values, callbackUrl)
                 .then((data) => {
                     if (data?.error) {
                         setError(data.error);
